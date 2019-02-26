94890a3d3f9e0c730eda6ac81992736e2c9e0b10
Nerea Pascual Diaz <nereapasc@gmail.com>
2019-02-26 16:14:11 +0100

    tuesday trial, not working

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,14 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  # read all
+  get 'tasks', to: 'tasks#index'
+  # read one
+  get 'tasks/:id', to: 'tasks#show', as: 'show'
+  # create
+  get 'tasks/new', to: 'tasks#new'
+  post 'tasks', to: 'tasks#create', as: 'create'
+  # edit/update
+  get 'tasks/:id/edit', to: 'tasks#edit'
+  patch 'tasks/:id', to: 'tasks#update'
+  # delete
+  delete "tasks/:id", to: "tasks#destroy"
 end
